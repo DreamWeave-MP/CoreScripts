9a532ff5eed7bba1e1e69a10c0252475c6c047c4
Phoenix / Hotaru <DeriousHD@gmail.com>
2023-10-02 05:15:35 +0100

    Simplify generateScriptID

--- scripts/customEventHooks.lua
+++ scripts/customEventHooks.lua
@@ -15,11 +15,8 @@ function customEventHooks.generateScriptID(filePath)
 	end
 	math.randomseed(os.time() + seed)
 
-	local template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
-	return string.gsub(template, "[xy]", function(c)
-		local v = (c == "x") and math.random(0, 15) or math.random(8, 11)
-		return string.format("%x", v)
-	end)
+    local template = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    return template:gsub("x", function() return string.format("%x", math.random(0, 15)) end)
 end
 
 
